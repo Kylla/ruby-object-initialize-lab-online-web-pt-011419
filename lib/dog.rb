9e23eb64bd98e4a9bde5fbedf3ea128866149e1b
Kylla Harris <kylla.harris@gmail.com>
2019-02-20 06:34:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog 
   def initialize(name, breed)
-    
+     @name = name 
+     @breed = breed
+  end
+ 
+ def name=(name)
+   @name = name
+ end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ def name
+   @name
+ end
+ 
+  def breed
+    puts "Mutt"
+  end
+end
